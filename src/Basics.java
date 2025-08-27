9346ec4c9991f1d2bf68d224a031ac0df0181cb9
phpontes <ppontes95@hotmail.com>
2025-08-27 09:14:28 -0300

    assertions on json response body and headers, moved body payload to separate file

--- src/Basics.java
+++ src/Basics.java
@@ -1,5 +1,8 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.Payload;
 
 public class Basics {
 
@@ -9,23 +12,9 @@ public class Basics {
 		//then - validate the response
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "    \"location\": {\r\n"
-				+ "        \"lat\": -23.5372836897518,\r\n"
-				+ "        \"lng\": -47.54972387771147\r\n"
-				+ "    },\r\n"
-				+ "    \"accuracy\": 50,\r\n"
-				+ "    \"name\": \"Tem Nada Aqui\",\r\n"
-				+ "    \"phone_number\": \"(+55) 15 220 2513\",\r\n"
-				+ "    \"address\": \"Rua dos Bobos, 0\",\r\n"
-				+ "    \"types\": [\r\n"
-				+ "        \"shoe park\",\r\n"
-				+ "        \"shop\"\r\n"
-				+ "    ],\r\n"
-				+ "    \"website\": \"https://google.com\",\r\n"
-				+ "    \"language\": \"French-IN\"\r\n"
-				+ "}")
+		.body(Payload.addPlace())
 		.when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);
+		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("server", "Apache/2.4.52 (Ubuntu)");
 	}
 }
---
+++ src/files/Payload.java
@@ -0,0 +1,23 @@
+package files;
+
+public class Payload {
+
+	public static String addPlace() {
+		return "{\r\n"
+				+ "    \"location\": {\r\n"
+				+ "        \"lat\": -23.5372836897518,\r\n"
+				+ "        \"lng\": -47.54972387771147\r\n"
+				+ "    },\r\n"
+				+ "    \"accuracy\": 50,\r\n"
+				+ "    \"name\": \"Tem Nada Aqui\",\r\n"
+				+ "    \"phone_number\": \"(+55) 15 220 2513\",\r\n"
+				+ "    \"address\": \"Rua dos Bobos, 0\",\r\n"
+				+ "    \"types\": [\r\n"
+				+ "        \"shoe park\",\r\n"
+				+ "        \"shop\"\r\n"
+				+ "    ],\r\n"
+				+ "    \"website\": \"https://google.com\",\r\n"
+				+ "    \"language\": \"French-IN\"\r\n"
+				+ "}";
+	}
+}
